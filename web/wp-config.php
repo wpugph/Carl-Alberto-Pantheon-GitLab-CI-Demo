773c8e2df42d867503b025bddf2a082ac104a406
carl alberto <carl.alberto@pantheon.io>
2018-12-02 14:47:03 +0800

    modified pantheon prepare scripts

--- web/wp-config.php
+++ web/wp-config.php
@@ -73,8 +73,10 @@ if ( ! isset( $_ENV['PANTHEON_ENVIRONMENT'] ) ):
 	/**
 	 * Set debug modes
 	 */
-	define( 'WP_DEBUG', getenv( 'WP_DEBUG' ) === 'true' ? true : false );
+	#define( 'WP_DEBUG', getenv( 'WP_DEBUG' ) === 'true' ? true : false );
 	define( 'IS_LOCAL', getenv( 'IS_LOCAL' ) !== false ? true : false );
+
+	define( 'WP_DEBUG', true );
 
 	/**#@+
 	 * Authentication Unique Keys and Salts.
@@ -153,7 +155,7 @@ if ( isset( $_ENV['PANTHEON_ENVIRONMENT'] ) ):
 			$scheme = 'https';
 		}
 		define( 'WP_HOME', $scheme . '://' . $_SERVER['HTTP_HOST'] );
-		define( 'WP_SITEURL', $scheme . '://' . $_SERVER['HTTP_HOST'] . '/wp' );
+		define( 'WP_SITEURL', $scheme . '://' . $_SERVER['HTTP_HOST'] . '/core' );
 
 	}
 	// Don't show deprecations; useful under PHP 5.5
@@ -176,12 +178,18 @@ endif;
 define( 'WP_CONTENT_DIR', dirname( __FILE__ ) . '/sites/default' );
 define( 'WP_CONTENT_URL', WP_HOME . '/sites/default' );
 
+#Set WP_CONTENT_URL to the full URI of this directory (no trailing slash), e.g.
+#define( 'WP_CONTENT_URL', 'http://example/blog/wp-content');
 #Set WP_PLUGIN_DIR to the full local path of this directory (no trailing slash), e.g.
 define( 'WP_PLUGIN_DIR', dirname( __FILE__ ) . '/sites/default/modules' );
 #Set WP_PLUGIN_URL to the full URI of this directory (no trailing slash), e.g.
-define( 'WP_PLUGIN_URL', WP_HOME . '/sites/default/modules');
+#define( 'WP_PLUGIN_URL', 'http://example/blog/wp-content/plugins');
 #If you have compability issues with plugins Set PLUGINDIR to the full local path of this directory (no trailing slash), e.g.
-define( 'PLUGINDIR', dirname( __FILE__ ) . '/sites/default/modules' );
+#define( 'PLUGINDIR', $_SERVER['DOCUMENT_ROOT'] . '/blog/wp-content/plugins' );
+
+echo 'test123:' . WP_PLUGIN_DIR;
+
+echo WP_CONTENT_URL;
 
 define( 'UPLOADS', '/sites/default/files' );
 
