2ad25db5319bad1034f950d96c4faef1a4baa977
carl alberto <carl.alberto@pantheon.io>
2018-12-02 13:06:28 +0800

    disable phpcs

--- scripts/test.sh
+++ scripts/test.sh
@@ -10,5 +10,5 @@ export PHPCS_DIR=$ROOTPWD/vendor/squizlabs/php_codesniffer/bin #phpcs v >3
 export SNIFFS_DIR=$ROOTPWD/vendor/wp-coding-standards/wpcs
 export THEMEIGNORE=*gulpfile.js*,*/assets/*,*.css*,*/gulp*.js,*.js*
 
-$PHPCS_DIR/phpcs --config-set installed_paths $SNIFFS_DIR
-$PHPCS_DIR/phpcs -p $THEMETOCHECK --standard=WordPress --ignore=$THEMEIGNORE
+#$PHPCS_DIR/phpcs --config-set installed_paths $SNIFFS_DIR
+#$PHPCS_DIR/phpcs -p $THEMETOCHECK --standard=WordPress --ignore=$THEMEIGNORE
